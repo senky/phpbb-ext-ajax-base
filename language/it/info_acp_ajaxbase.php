e26bf8eb8ebdaef7c5e56221fa220e102b80b24b
Jakub Senko <jakubsenko@gmail.com>
2020-02-21 10:14:03 +0100

    Add IT translation

---
+++ language/it/info_acp_ajaxbase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* Ajax Base extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2016 Jakub Senko <jakubsenko@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_AJAX_BASE_TITLE'			=> 'Ajax Base',
+	'ACP_AJAX_BASE_OPTIONS_SAVED'	=> 'Le opzioni Ajax Base sono state salvate.',
+	'AJAXBASE_ALLOW_PREVIEW'		=> 'Anteprima del messaggio Ajax',
+	'AJAXBASE_ALLOW_WHOISONLINE'	=> 'Sezione di chi è collegato Ajax',
+	'AJAXBASE_ALLOW_STATISTICS'		=> 'Statistiche Ajax',
+));
